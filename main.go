14a345db2ef74633360ba0183e403e311c52c6e3
Matthew Brown <me@matthewbrown.io>
2016-01-07 17:18:24 +1000

    Added base CLI

--- main.go
+++ main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"github.com/codegangsta/cli"
+	"os"
+	"path/filepath"
 )
 
+func walkFiles(source string, ignore string) ([]string, error) {
+	var items []string
+	err := filepath.Walk(source, func(p string, fi os.FileInfo, err error) error {
+		if err != nil || fi.IsDir() {
+			return err
+		}
+		rel, err := filepath.Rel(source, p)
+		if err != nil {
+			return err
+		}
+
+		var ignoreFile bool
+		if ignore != "" {
+			ignoreFile, err = filepath.Match(ignore, rel)
+		}
+		if err != nil || ignoreFile {
+			return err
+		}
+		items = append(items, p)
+		return nil
+	})
+	return items, err
+}
+
 func main() {
-	fmt.Println("A go implementation of Azure's blobxfer")
+	app := cli.NewApp()
+	app.Name = "blobxfer"
+	app.Usage = "Upload files to Azure Blob Storage"
+	app.Author = "Matthew Brown <me@matthewbrown.io>"
+	app.Version = "0.1.0"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "storageaccountkey",
+			Value:  "",
+			Usage:  "The storage account key to use",
+			EnvVar: "STORAGE_ACCOUNT_KEY",
+		},
+	}
+	app.Run(os.Args)
 }
